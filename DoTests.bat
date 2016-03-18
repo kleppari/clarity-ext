faaf620bd1d4a19c0e9a7001edfe8c4516f0ec1d
withrocks <withrocks@users.noreply.github.com>
2016-03-18 10:45:47 +0100

    Cleanup

--- DoTests.bat
+++
@@ -1,2 +0,0 @@
-py -2 -m clarity_ext.automated_tests.test_cases.dilute_tests.hamilton_tests2
-py -2 -m clarity_ext.automated_tests.test_cases.dilute_file_reader.hamilton_driver_file_tests
--- validate.sh
+++ validate.sh
@@ -1,2 +1,1 @@
-python -m clarity_ext.automated_tests.test_cases.dilute_tests.hamilton_tests2
-python -m clarity_ext.automated_tests.test_cases.dilute_file_reader.hamilton_driver_file_tests
+nosetests
